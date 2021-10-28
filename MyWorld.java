1c465e72a135367404232c55f77d6507fa7e5451
Aninda S <93136737+monkey123man@users.noreply.github.com>
2021-10-28 09:19:34 -0400

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -8,21 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    public int death = 0;
+    public int health = 0;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
-    {
+    {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000, 700, 1);
-        hangman deadman = new hangman();
-        addObject( deadman, 300, 200);
-    }
-    public void updatehang(){
-        if (death == 0){
-            
-        }
+        super(1000, 600, 1); 
+        
+        Person guy = new Person();
+        addObject(guy, 300, 200);
     }
 }
---
+++ Person.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person extends Actor
+{
+    GreenfootImage[] personLives = new GreenfootImage[7];
+
+    public Person()
+    {
+        for(int i = 0; i <=6; i++)
+        {
+            personLives[i] = new GreenfootImage("images/person/person" + i);
+        }
+    }
+    /**
+     * Act - do whatever the Person wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        move(1);
+        
+        MyWorld world = new MyWorld();
+        setImage(personLives[world.health]);
+    }
+}
