f91f015907c9159be9bf5f7b746ef22f7098a9e5
Aninda S <93136737+monkey123man@users.noreply.github.com>
2021-10-28 09:46:43 -0400

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int health = 0;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,9 +16,12 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000, 700, 1); 
+        super(1000, 600, 1); 
         
-        Person guy = new Person();
-        addObject(guy, 300, 200);
+        HangMan guy = new HangMan();
+        addObject(guy, 310, 190);
+        
+        Label guess = new Label("Guesses!", 85);
+        addObject(guess, 820, 50);
     }
 }
