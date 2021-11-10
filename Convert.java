5aa5a5ab7240391704e30044993c405d71e48153
Johnson349 <93137591+Johnson349@users.noreply.github.com>
2021-11-10 22:49:39 +0800

    Create Convert.java

---
+++ Convert.java
@@ -0,0 +1,13 @@
+/** Covert between String and Characters.
+*
+* Stewart Chan
+*
+*/
+
+// Convert a String into an array of char
+String word = "hello";
+char[] chars = word.toCharArray();
+
+// Convert an array of char into a String
+char[] chars = {'h', 'e', 'l', 'l', 'o'};
+String str = new String(chars);
